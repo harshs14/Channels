4edb121f297ce79f4439ff3ed5facf9d1196bd00
harshs14 <harsh.singh1411@gmail.com>
2019-02-01 17:46:23 +0530

    adding consumers

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,11 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ASGI_APPLICATION = 'mysite.routing.application'
 
 # Application definition
 
 INSTALLED_APPS = [
-    'chat'
+    'chat',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
